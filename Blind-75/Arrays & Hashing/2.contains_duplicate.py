0180e2e7134d002dfbae76fecdb85b564e9acaac
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-06-30 07:35:11 -0700

    chore: adding data

---
+++ Blind-75/Arrays & Hashing/2.contains_duplicate.py
@@ -0,0 +1,54 @@
+# Contains Duplicate
+
+"""
+Notes:
+- If the value appears more than once, return true.
+- If the value appears only once, return false.
+"""
+
+from typing import List
+
+class Solution:
+    # O(n^2) time | O(1) space - where n is the length of the array
+    def containsDuplicate(self, nums: List[int]) -> bool: # time exceeded
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False # time limit exceeded on leetcode
+
+    # O(n) time | O(n) space - where n is the length of the array
+    def containsDuplicate_set(self, nums: List[int]) -> bool:
+        """
+        A set is a collection of: 
+        unordered: no duplicates
+        unchangeable: no defined order
+        unindexed: cannot change the items after the set has been created
+        """
+        seen = set() # set to store seen values
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+    # or 
+
+    def containsDuplicate_short(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+    # O(nlogn) time | O(n) space - where n is the length of the array
+    def containsDuplicate_sort(self, nums: List[int]) -> bool:
+        nums.sort()
+        
+        for i in range(0,len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        
+        return False
+
+
+print(Solution().containsDuplicate([1,2,3,1]))
+print(Solution().containsDuplicate_set([1,2,3,1]))
+print(Solution().containsDuplicate_sort([1,2,3,1]))
+print(Solution().containsDuplicate_short([1,2,3,1]))
--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 - Work Smart NOT Hard
 - Why solve 2000+ LC problems when you can ONLY solve 75-200 LeetCode patterns?
 
-## Two curated List of LeetCode-Patterns:
+## Two curated Lists of LeetCode-Patterns:
 - Blind-75:
-    * Blind-75 are 75 LeetCode problems created by Tech-Leads and most widely accepted by the community from leetcode and are the most commonly used leetcode problems.
+    * Blind-75 are 75 LeetCode problems created by Tech-Leads and most widely accepted by the community from leetcode and are the most commonly used leetcode patterns.
 
 -  Comprehensive-List:
     * Comprehensive-List is an extensive list of ~200 LeetCode problems I picked up from leetcode. It includes all the common LeetCode patterns from Blind 75 and some extra additions to practice.
