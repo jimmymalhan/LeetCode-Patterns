ce808e5b9c763d473591e39d0789ed5693f9f10a
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-07-02 07:49:54 -0700

    chore: adding data

---
+++ Blind-75/Arrays & Hashing/3.valid_anagram.py
@@ -0,0 +1,48 @@
+# Valid Anagram
+"""
+Notes:
+- if the two strings are the same length, then they must be anagrams
+- create a dictionary to store the characters and their counts
+- iterate through the first string and add the characters to the dictionary
+- iterate through the second string and subtract the characters from the dictionary
+- if the dictionary is empty, then the strings are anagrams
+
+test case1 = 'anagram', 'nagaram'
+{'a': 1}
+{'a': 1, 'n': 1}
+{'a': 2, 'n': 1}
+{'a': 2, 'n': 1, 'g': 1}
+{'a': 2, 'n': 1, 'g': 1, 'r': 1}
+{'a': 3, 'n': 1, 'g': 1, 'r': 1}
+{'a': 3, 'n': 1, 'g': 1, 'r': 1, 'm': 1}
+
+test case2 = 'rat', 'car'
+{'r': 1}
+{'r': 1, 'a': 1}
+{'r': 1, 'a': 1, 't': 1}
+"""
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): # if the length of the two strings are not equal, they cannot be anagrams
+            return False
+        d = {}
+        for character in s:
+            d[character] = d.get(character, 0) + 1 # get(character, 0) returns 0 if character is not in d
+
+        for character in t:
+            if character not in d: 
+                return False
+            d[character] -= 1 # decrement the count of c in d
+            if d[character] < 0: # if the count of c in d is negative, return False
+                return False
+        return True
+
+    def isAnagram2(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+print(Solution().isAnagram("anagram", "nagaram")) # True
+print(Solution().isAnagram("rat", "car")) # False
+print(Solution().isAnagram2("anagram", "nagaram")) # True
+print(Solution().isAnagram2("rat", "car")) # False
