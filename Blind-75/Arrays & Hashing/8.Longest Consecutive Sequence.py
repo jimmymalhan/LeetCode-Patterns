b09128353efe11d48ec78cebc797b25634794006
Jimmy Malhan <jimmymalhan999@gmail.com>
2022-08-23 12:03:50 -0700

    chore: adding data

--- Blind-75/Arrays & Hashing/8.Longest Consecutive Sequence.py
+++ Blind-75/Arrays & Hashing/8.Longest Consecutive Sequence.py
@@ -2,8 +2,13 @@
 
 """
 Question Explanation:
+- Look at the sequence of consecutive numbers in the array and find the longest sequence of consecutive numbers.
+- how to identify what is sequence? 
+    - Check the start value from looking into the left neighbor
+    - easiest way is to use set
 
 Solution Explanation:
+- Brute force solution: By sorting it - Time Complexity: O(nlog(n))
 1. We create a set of the numbers in the array.
 2. We loop through the set and check if the number - 1 is in the set.
 3. If it is not, then we know that it is the beginning of a sequence.
@@ -15,22 +20,24 @@ Solution Explanation:
 
 from typing import List  
 
+# O(n) time | O(1) space - where n is the length of the array coz we are looping through the array once & set provides the lookup space of O(1).
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if not nums: return 0
-
-        numSet = set(nums)
+        
+        # nums.sort()
+        nums = set(nums)
         longest = 0
 
-        for num in numSet:
-            # Checking to see if it's the beginning of a sequence
-            if (num - 1) not in numSet:
+        for num in nums:
+            # Checking to see if it's the beginning of a sequence or does it have a left neighbor.
+            if (num - 1) not in nums:
                 length = 1
                 # Checking to see if it's the end of a sequence
-                while (num + length) in numSet:
+                while (num + length) in nums:
                     length += 1
                 longest = max(longest, length)
         return longest
 
-print(Solution().longestConsecutive([100, 4, 200, 1, 3, 2]))
-print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
+print(Solution().longestConsecutive([100, 4, 200, 1, 3, 2])) # 4
+print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1])) # 9
